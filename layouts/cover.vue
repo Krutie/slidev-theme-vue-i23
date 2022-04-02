e47c5dc9080dd9d06176d5d7bd34a9d05600391f
Krutie Patel <krutie.patel@gmail.com>
2022-04-02 23:14:16 +1000

    fix: add missing class directly in the layout to prevent an additional front-matter data

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="slidev-layout cover text-center" :class="class" :style="style">
+    <div class="slidev-layout text-center cover" :class="class" :style="style">
         <div class="my-auto w-full">
             <slot />
         </div>
