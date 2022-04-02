077aea43aafb19f10750f745623cbacebb3e40eb
Krutie Patel <krutie.patel@gmail.com>
2022-04-02 23:13:35 +1000

    fix: replace 'content' layout with 'default'

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="slidev-layout default" :class="class" :style="style">
+        <div class="mx-auto w-full">
+            <slot />
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "@slidev/client/layoutHelper";
+
+const props = defineProps({
+    image: {
+        type: String,
+    },
+    class: {
+        type: String,
+    },
+});
+
+const style = computed(() => handleBackground(props.image));
+</script>
